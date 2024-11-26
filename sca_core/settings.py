80584a2e0a24dd732bcaca56e78817fdd5f916e9
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-26 15:02:53 +0200

    add debug toolbar

--- sca_core/settings.py
+++ sca_core/settings.py
@@ -32,6 +32,8 @@ DEBUG = os.getenv("DJANGO_DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = ["127.0.0.1", "localhost"]
+
 
 # Application definition
 
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 
     # Third-party apps
     "rest_framework",
+    "debug_toolbar",
 
     # Custom apps
     "spy_agency",
@@ -52,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
