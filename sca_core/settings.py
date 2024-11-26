0757839634b5fd5b568b6c9e4e9f05a950012fe0
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-26 16:18:18 +0200

    add API_URL for settings

--- sca_core/settings.py
+++ sca_core/settings.py
@@ -141,3 +141,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Breed validation API url
+API_URL = os.getenv("API_URL")
