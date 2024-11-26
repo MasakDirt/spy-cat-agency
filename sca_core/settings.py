252c80901836bdac9c59110b004e486352c5407e
maksym_korniev <maksymkorniev88@gmail.com>
2024-11-26 14:20:32 +0200

    add postgres docker image and refactor settings for postgres connection

--- sca_core/settings.py
+++ sca_core/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n8tpe&ql)!uj$*e@lo4n%_1xj+8#h13%2mq)ngb$7&101+dlg3"
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
-# SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv("DJANGO_DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -42,7 +47,7 @@ INSTALLED_APPS = [
     "rest_framework",
 
     # Custom apps
-    "spa_agency",
+    "spy_agency",
 ]
 
 MIDDLEWARE = [
@@ -82,8 +87,12 @@ WSGI_APPLICATION = "sca_core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
